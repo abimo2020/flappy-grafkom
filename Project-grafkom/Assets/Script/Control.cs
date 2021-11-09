3d5b137d933e976b6fd9c2fd0d5adf31002c24e9
Aryo Bimo <abimo2018@gmail.com>
2021-11-09 21:12:38 +0700

    Menambahkan backsound dan sound effect, edit top line dan bottom line

--- Project-grafkom/Assets/Script/Control.cs
+++ Project-grafkom/Assets/Script/Control.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Control : MonoBehaviour
 {
     public GameObject instruksi,asteroid,text, gameover;
+    public AudioSource suaraScore, suaraJatuh, suaraTouch;
     Rigidbody2D Rb;
     public float jumpForce;
     float score;    
@@ -26,6 +27,7 @@ public class Control : MonoBehaviour
         scoreTxt.text = "Score : " + score;
         if (Input.GetMouseButtonDown(0))
         {
+            suaraTouch.Play();
             text.SetActive(true);
             Rb.gravityScale=3;
             asteroid.SetActive(true);
@@ -37,10 +39,12 @@ public class Control : MonoBehaviour
     {
         if (collision.gameObject.tag == "point")
         {
+            suaraScore.Play();
             score++;
         }
         if(collision.gameObject.tag == "asteroid" || collision.gameObject.tag=="bottom")
         {
+            suaraJatuh.Play();
             Destroy(gameObject);
             gameover.SetActive(true);
             asteroid.SetActive(false);
--- Project-grafkom/Assets/Script/asteroidScript.cs
+++ Project-grafkom/Assets/Script/asteroidScript.cs
@@ -8,13 +8,13 @@ public class asteroidScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        nilaiRandom = Random.Range(-18.9f, 6.8f);
+        nilaiRandom = Random.Range(-12.8f, 5f);
         transform.position = new Vector2(transform.position.x, nilaiRandom);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, Vector2.left*100, Time.deltaTime*7);
+        transform.position = Vector2.MoveTowards(transform.position, Vector2.left*50, Time.deltaTime*7);
     }
 }
